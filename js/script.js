778df9ef34098ed31630d295ae6af06f9d5a16d5
Bartłomiej Zimny <bart.zimny@gmail.com>
2019-09-18 23:28:25 +0200

    To fix js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const titleClickHandler = function(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
   console.log('Event: ', event);
@@ -7,30 +8,40 @@ const titleClickHandler = function(event){
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
 
   /* add class 'active' to the clicked link */
-
-  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+  console.log('Add active to clickedElement:', clickedElement);
+  
 
   /* remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.posts .active');
 
-for(let activeArticle of activeArticles){
-  activeArticle.classList.remove('active');
-}
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('clicked href:', articleSelector);
+ 
+
   /* find the correct article using the selector (value of 'href' attribute) */
+
+  
+  const targetArticle = document.querySelector('.post a');
+  console.log('value of href:', targetArticle);
 
   /* add class 'active' to the correct article */
 
-  clickedElement.classList.add('active');
-
+ 
+  targetArticle.classList.add('active');
+  console.log('active article:', targetArticle);
 
 }
 
